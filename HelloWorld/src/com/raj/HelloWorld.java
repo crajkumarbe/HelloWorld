8d64c04c18e8c1f8fb90e289203bbb7697022455
rajkumar.c <rajkumar.c@laptop100>
2015-02-17 16:53:14 +0530

    Fixing the 2nd sysout statement as per Nivedita's request.

--- HelloWorld/src/com/raj/HelloWorld.java
+++ HelloWorld/src/com/raj/HelloWorld.java
@@ -8,7 +8,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Java Hello World !!!");
-		System.out.println("Adding one more line to the existing program!!!");
 		System.out.println("Tested the sample hello world application");
 		System.out.println("Succcesfully committed the code in the github repository!!!");
 		System.out.println("Working with Sharan!!");
